264666a87336e6f87ce92ea67caad6a51c1ffc93
wefine <wang.xiaoren@zte.com.cn>
2017-02-08 16:31:49 +0800

    step-2, define a controller and binding data.
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- angularjs-phonecat/app/app.js
+++ angularjs-phonecat/app/app.js
@@ -1,11 +1,20 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+// Define the `phonecatApp` module
+var phonecatApp = angular.module('phonecatApp', []);
 
-  $routeProvider.otherwise({redirectTo: '/'});
-}]);
+// Define the `PhoneListController` controller on the `phonecatApp` module
+phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
+    $scope.phones = [
+        {
+            name: 'Nexus S',
+            snippet: 'Fast just got faster with Nexus S.'
+        }, {
+            name: 'Motorola XOOM™ with Wi-Fi',
+            snippet: 'The Next, Next Generation tablet.'
+        }, {
+            name: 'MOTOROLA XOOM™',
+            snippet: 'The Next, Next Generation tablet.'
+        }
+    ];
+});
