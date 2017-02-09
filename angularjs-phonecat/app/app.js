261803812ad401b036bf2fc6abddcc8d3f1fab53
wefine <wang.xiaoren@zte.com.cn>
2017-02-09 19:22:52 +0800

    step-3, use component to render
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- angularjs-phonecat/app/app.js
+++ angularjs-phonecat/app/app.js
@@ -2,21 +2,3 @@
 
 // Define the `phonecatApp` module
 var phonecatApp = angular.module('phonecatApp', []);
-
-// Define the `PhoneListController` controller on the `phonecatApp` module
-phonecatApp.controller('PhoneListController', function($scope) {
-    $scope.phones = [
-        {
-            name: 'Nexus S',
-            snippet: 'Fast just got faster with Nexus S.'
-        }, {
-            name: 'Motorola XOOM™ with Wi-Fi',
-            snippet: 'The Next, Next Generation tablet.'
-        }, {
-            name: 'MOTOROLA XOOM™',
-            snippet: 'The Next, Next Generation tablet.'
-        }
-    ];
-
-    $scope.hello = "Hello, Angular!";
-});
