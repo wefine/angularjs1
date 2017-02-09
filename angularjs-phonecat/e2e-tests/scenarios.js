48083c8a089df8fd42a4692c0cc5bfca86a9aaea
wefine <wang.xiaoren@zte.com.cn>
2017-02-10 07:56:51 +0800

    step-8, Templating Links & Images
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- angularjs-phonecat/e2e-tests/scenarios.js
+++ angularjs-phonecat/e2e-tests/scenarios.js
@@ -52,6 +52,13 @@ describe('PhoneCat Application', function () {
             ]);
         });
 
+        it('should render phone specific links', function () {
+            var query = element(by.model('$ctrl.query'));
+            query.sendKeys('nexus');
+
+            element.all(by.css('.phones li a')).first().click();
+            expect(browser.getLocationAbsUrl()).toBe('/phones/nexus-s');
+        });
     });
 
 });
