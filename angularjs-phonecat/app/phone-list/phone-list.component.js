d5511935bdf68c86981c738a331f751a0368c2c6
wefine <wang.xiaoren@zte.com.cn>
2017-02-10 08:45:54 +0800

    step-9, Routing & Multiple Views
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- angularjs-phonecat/app/phone-list/phone-list.component.js
+++ angularjs-phonecat/app/phone-list/phone-list.component.js
@@ -3,12 +3,12 @@
 // Register `phoneList` component, along with its associated controller and template
 angular.module('phoneList').component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController($http) {
+    controller: ['$http', function PhoneListController($http) {
         var self = this;
         self.orderProp = 'age';
 
         $http.get('phones/phones.json').then(function (response) {
             self.phones = response.data;
         });
-    }
+    }]
 });
