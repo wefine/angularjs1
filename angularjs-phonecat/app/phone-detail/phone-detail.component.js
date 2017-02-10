9a12b4d4a82a3f71839f560168e97e3507fc6203
wefine <wang.xiaoren@zte.com.cn>
2017-02-10 13:56:42 +0800

    step-10, More Templating
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- angularjs-phonecat/app/phone-detail/phone-detail.component.js
+++ angularjs-phonecat/app/phone-detail/phone-detail.component.js
@@ -2,10 +2,14 @@
 
 // Register `phoneDetail` component, along with its associated controller and template
 angular.module('phoneDetail').component('phoneDetail', {
-    template: 'TBD: Detail view for <span>{{$ctrl.phoneId}}</span>',
-    controller: ['$routeParams',
-        function PhoneDetailController($routeParams) {
-            this.phoneId = $routeParams.phoneId;
+    templateUrl: 'phone-detail/phone-detail.template.html',
+    controller: ['$http', '$routeParams',
+        function PhoneDetailController($http, $routeParams) {
+            var self = this;
+
+            $http.get('phones/' + $routeParams.phoneId + '.json').then(function (response) {
+                self.phone = response.data;
+            });
         }
     ]
 });
