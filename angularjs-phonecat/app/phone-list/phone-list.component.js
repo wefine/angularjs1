fed35f91ceea7e26a6b488dbb475ab5dd9a5185a
wefine <wang.xiaoren@zte.com.cn>
2017-02-13 09:23:20 +0800

    step-13, REST and Custom Services
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- angularjs-phonecat/app/phone-list/phone-list.component.js
+++ angularjs-phonecat/app/phone-list/phone-list.component.js
@@ -3,12 +3,10 @@
 // Register `phoneList` component, along with its associated controller and template
 angular.module('phoneList').component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['$http', function PhoneListController($http) {
-        var self = this;
-        self.orderProp = 'age';
-
-        $http.get('phones/phones.json').then(function (response) {
-            self.phones = response.data;
-        });
-    }]
+    controller: ['Phone',
+        function PhoneListController(Phone) {
+            this.phones = Phone.query();
+            this.orderProp = 'age';
+        }
+    ]
 });
